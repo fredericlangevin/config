4212fe2e7e6119f44d1bed38546235e1c1dbcf3b
Frederic Langevin <frederic.langevin@coopengo.com>
2016-12-11 13:41:58 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+rm ~/.bashrc
+ln -s ~/.my_dot_config/bashrc ~/.bashrc
+rm ~/.bash_aliases
+ln -s ~/.my_dot_config/bash_aliases ~/.bash_aliases
+rm ~/.gitconfig
+ln -s ~/.my_dot_config/gitconfig ~/.gitconfig
